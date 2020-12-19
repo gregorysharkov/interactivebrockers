2e5533d51cb303ec1a66baca91ecfce2907daa5d
gregorysharkov <33329613+gregorysharkov@users.noreply.github.com>
2020-12-19 14:58:05 +0100

    Hello world (#1)
    * version 1 of hello world app is done
    
    * now the hello world is workign properly

---
+++ hello_world/hello_world.py
@@ -0,0 +1,36 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+
+
+class TestApp(EWrapper,EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    
+    def contractDetails(self, reqId, contractDetails):
+        print("contractDetails: ", reqId, " ", contractDetails)
+
+
+def main():
+    app = TestApp()
+    app.connect("127.0.0.1", 7497, 0)
+
+    contract = Contract()
+    contract.symbol = "NVDA"
+    contract.secType = "STK"
+    contract.exchange = "SMART"
+    contract.currency = "USD"
+    contract.primaryExchange = "NASDAQ"
+
+    app.reqContractDetails(0, contract)
+    print(contract)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
