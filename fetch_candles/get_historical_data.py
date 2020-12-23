3ca3d3293c174d8bd389254e6e8a9948eb4b636e
Grigory Sharkov <gregorysharkov@gmail.com>
2020-12-23 23:37:42 +0100

    historical fetching is working properly

--- fetch_candles/get_historical_data.py
+++ fetch_candles/get_historical_data.py
@@ -11,16 +11,20 @@ class TestApp(EWrapper,EClient):
 
     def __init__(self):
         EClient.__init__(self, self)
-
+        self.filename = "fetch_candles\\data\\EurGbpFx.txt"
+        #clear the contents of the file
+        f = open(self.filename, 'r+')
+        f.truncate(0)
+        f.close()
     
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, " ", errorCode, " ", errorString)
 
     
     def historicalData(self, reqId:int, bar: BarData):
-        print(bar)
-        with open(".data/EurGbpFx.txt", mode="a+") as f:
-            f.write(bar)
+        print(str(bar))
+        with open(self.filename, mode="a") as f:
+            f.write(str(bar)+"\n")
             f.close()
     
     def contractDetails(self, reqId, contractDetails):
@@ -34,13 +38,13 @@ def main():
     app.connect("127.0.0.1", 7497, 0)
 
     app.reqHistoricalData(
-        1, 
+        1,
         ContractSamples.EurGbpFx(), 
         "", 
-        "1 D", 
+        "2 D", 
         "1 min", 
         "MIDPOINT", 
-        1, 
+        0, 
         1, 
         False,
         []
